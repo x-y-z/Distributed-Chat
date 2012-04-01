24a1d72ac37bfaf7022c3fb97510c859f501e745
Zi Yan <birdmayan@gmail.com>
2012-04-01 16:07:58 -0400

    udp changes

--- src/udp/udp.h
+++ src/udp/udp.h
@@ -1,6 +1,25 @@
 #ifndef __UDP_H__
 #define __UDP_H__
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+class UDP
+{
+public:
+    UDP(){};
+    ~UDP(){};
+
+private:
+    int _socket;
+    struct sockaddr_in _address;
+    struct sockaddr_in _peer_list;
+
+}
 
 
 #endif
