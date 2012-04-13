3a7b7224bd158575c6a11bbf3512c731f4339041
Zi Yan <birdmanyan@gmail.com>
2012-04-13 13:01:22 -0400

    added sequencer

---
+++ src/sequencer/sequencer.cpp
@@ -0,0 +1,16 @@
+// ===================================================================
+// 
+//       Filename:  sequencer.cpp
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  04/13/2012 12:19:23
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Zi Yan (yz), zi.yan@gmx.com
+//        Company:  
+// 
+// ===================================================================
+#include "sequencer.h"
---
+++ src/sequencer/sequencer.h
@@ -0,0 +1,26 @@
+// ===================================================================
+// 
+//       Filename:  sequencer.h
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  04/13/2012 12:18:43
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Zi Yan (yz), zi.yan@gmx.com
+//        Company:  
+// 
+// ====================================================================
+#ifndef __SEQUENCER_H__
+#define __SEQUENCER_H__
+
+
+class sequencer
+{
+
+};
+
+
+#endif
