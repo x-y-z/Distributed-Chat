695260add39e953d6d5686ce1dcd23208aa07504
Xin Dong <xindon@HULA.seas.upenn.edu>
2012-04-17 16:50:46 -0400

    new funcs added

--- src/chatClient/chatClient.h
+++ src/chatClient/chatClient.h
@@ -53,7 +53,9 @@ public:
     int msgEnqueue(string msg);
     int sendBroadcastMsg(string msgContent);
     void displayClients();
-    int dojoin(string s_ip, int s_port);    
+    int dojoin(string s_ip, int s_port); 
+    int getID();
+    vector<peer> getClientList();   
 private:
     int addNewUser(string name, string newCIP, int newCPort, int newCID);
     int removeUser(int CID);
